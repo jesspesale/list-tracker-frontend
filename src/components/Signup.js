766a168332081c458143457a0d8e49c93c323015
jesspesale <jess_pesale@aol.com>
2022-02-28 01:33:29 -0500

    added user sign up form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -6,7 +6,15 @@ export default class Signup extends Component {
       <div>
         <h2>Signup Below:</h2>
         <form>
-            
+            <label>Email</label>
+          <input type="text" />
+          <br />
+          <label>Password</label>
+          <input type="text" />
+          <br />
+          <label>Confirm Password</label>
+          <input type="text" />
+          <button>Sign Up</button>
         </form>
       </div>
     );
