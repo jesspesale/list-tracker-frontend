4db6d1ab838232bc3e3626a7386787523e35f0da
jesspesale <jess_pesale@aol.com>
2022-02-28 00:44:34 -0500

    added react-router-dom and started routes for home and dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import React from "react"
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>React App</h1>
+      <Router>
+        <Routes>
+          {/* <Route exact path="/" component={Home} /> */}
+          {/* <Route exact path="/dashboard" component={Home} /> */}
+        </Routes>
+        <h1>React App</h1>
+      </Router>
     </div>
   );
 }
