fef233c9c79af1554e87d8af6edc2d4a83934ac7
jesspesale <jess_pesale@aol.com>
2022-02-28 19:39:16 -0500

    added redux and thunk dependencies

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -6,6 +6,12 @@ export default function Signup() {
     const [password, setPassword] = useState("");
     const [passwordConfirmation, setPasswordConfirmation] = useState("");
 
+    const state = {
+        email: email,
+        password: password,
+        passwordConfirmation: passwordConfirmation
+    }
+
     const handleEmailChange = (e) => {
         setEmail(e.target.value);
     };
@@ -18,10 +24,15 @@ export default function Signup() {
       setPasswordConfirmation(e.target.value);
     };
 
+    const handleOnSubmit = (e) => {
+        e.preventDefault()
+        console.log(state)
+    }
+
   return (
     <div>
       <h2>Signup Below:</h2>
-      <form>
+      <form onSubmit={handleOnSubmit}>
         <label>Email</label>
         <input type="text" value={email} onChange={handleEmailChange}/>
         <br />
@@ -30,7 +41,8 @@ export default function Signup() {
         <br />
         <label>Confirm Password</label>
         <input type="text" value={passwordConfirmation} onChange={handlePasswordConfirmChange} />
-        <button>Sign Up</button>
+        <br/>
+        <button type="submit">Sign Up</button>
       </form>
     </div>
   );
