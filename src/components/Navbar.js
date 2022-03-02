ea10a148976e86eff0e24c2296e87f7321061133
jesspesale <jess_pesale@aol.com>
2022-03-01 23:05:48 -0500

    connected navbar with logout action

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {logout} from ".././redux/actions/authActions"
+import { connect } from 'react-redux';
 
-export default function Navbar() {
-  return (
-    <div className="navbar">
-      <Link className="nav-item" to="/">
-        Home
-      </Link>
-      <Link className="nav-item" to="/dashboard">
-        Dashboard
-      </Link>
-      <Link className="nav-item" to="/login">
-        Login
-      </Link>
-      <Link className="nav-item" to="/signup">
-        Signup
-      </Link>
-    </div>
-  );
+class Navbar extends React.Component {
+
+    render(){
+        return (
+            <div className="navbar">
+                <Link className="nav-item" to="/">
+                Home
+                </Link>
+                <Link className="nav-item" to="/dashboard">
+                Dashboard
+                </Link>
+                <Link className="nav-item" to="/login">
+                Login
+                </Link>
+                <Link className="nav-item" to="/signup">
+                Signup
+                </Link>
+                <Link className="nav-item" onClick={() => this.props.logout} to="/">
+                Logout88
+                </Link>
+            </div>
+        );
+    }  
 }
+
+export default connect(null, {logout})(Navbar)
