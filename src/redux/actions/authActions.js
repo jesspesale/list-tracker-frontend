271f24bc62685494b692f9a9917ede6d69c1311f
jesspesale <jess_pesale@aol.com>
2022-03-01 20:50:23 -0500

    added login action to same auth successful reducer

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -8,16 +8,38 @@ export const signup = (user, history) => {
           body: JSON.stringify({user: user}),
         })
           .then((resp) => resp.json())
-          .then((data) => {  
-            console.log(data)       
-          dispatch({ 
-              type: "USER_SIGNUP", 
-              payload: {
-                  loggedIn: data.logged_in, 
-                  currentUser: data.user
-                } 
-              })
+          .then((data) => {    
+          dispatch({
+            type: "AUTH_SUCCESSFUL",
+            payload: {
+              loggedIn: data.logged_in,
+              currentUser: data.user,
+            },
+          });
               history.push('/dashboard')
           });
     }
 }
+
+export const login = (user, history) => {
+  return (dispatch) => {
+    fetch("http://localhost:3001/sessions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ user: user }),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        dispatch({
+          type: "AUTH_SUCCESSFUL",
+          payload: {
+            loggedIn: data.logged_in,
+            currentUser: data.user,
+          },
+        });
+        history.push("/dashboard");
+      });
+  };
+};
--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -1,14 +1,14 @@
 export default (state = {loggedIn: false, currentUser: {}}, action) => {
-    switch(action.type){
-        case "USER_SIGNUP":
-            // console.log(state, action)
-            return {
-                ...state, 
-                loggedIn: action.payload.loggedIn, 
-                currentUser: action.payload.currentUser
-            }
-            
-        default:
-                return state
+    switch (action.type) {
+      case "AUTH_SUCCESSFUL":
+        // console.log(state, action)
+        return {
+          ...state,
+          loggedIn: action.payload.loggedIn,
+          currentUser: action.payload.currentUser,
+        };
+
+      default:
+        return state;
     }
 }
