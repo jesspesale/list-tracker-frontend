b65631988b1744a31ba615b4ab71960ea0aa48bf
jesspesale <jess_pesale@aol.com>
2022-03-01 20:08:52 -0500

    connected dashboard component to store w/ mapStateToProps

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { connect } from 'react-redux';
 
-export default function Dashboard() {
+function Dashboard() {
   return (
     <div>
       <h2>Dashboard</h2>
     </div>
   );
+
+  const mapStateToProps = (state) => {
+    return {
+      user: state.auth.currentIser
+    }
+  }
 }
+export default connect(mapStateToProps)(Dashboard)
